ddfe3560ea418166da6c49767abdc39d955b1bed
lrbrr <sauravshah.mail@gmail.com>
2021-03-21 19:32:07 +0530

    Coding style changes

--- git_common_workflow.py
+++ git_common_workflow.py
@@ -11,10 +11,12 @@ ch = int(input('Enter the choice to proceed: '))
 
 
 def current_branch_name():
-    proc=subprocess.Popen('git branch --show-current', shell=True, stdout=subprocess.PIPE, )
-    output=proc.communicate()[0]
+    proc = subprocess.Popen('git branch --show-current',
+                            shell=True, stdout=subprocess.PIPE, )
+    output = proc.communicate()[0]
     branch_merge_name = str(output)[2:-3]
     return branch_merge_name
+
 
 if ch == 1:
     branch_name = input('Enter the branch name: ')
@@ -22,7 +24,7 @@ if ch == 1:
     switch = input('Do you want to switch to newly created branch: "y/n" ')
     if switch == 'y' or switch == 'Y':
         os.system(f'git checkout {branch_name}')
-    print('You are working at: ', end = '')
+    print('You are working at: ', end='')
     os.system('git branch --show-current')
 
 elif ch == 2:
@@ -31,7 +33,7 @@ elif ch == 2:
     os.system(f'git checkout {where_to_work}')
 
 elif ch == 3:
-    print('You are working at: ', end = '')
+    print('You are working at: ', end='')
     os.system('git branch --show-current')
     push = input('Confirm to push into remote: "y/n"')
     if push == 'y' or push == 'Y':
@@ -45,7 +47,7 @@ elif ch == 3:
         os.system(f'git push -u origin {branch_name_string}')
 
 elif ch == 4:
-    print('You are at: ', end = '')
+    print('You are at: ', end='')
     os.system('git branch --show-current')
 
     branch_merge_name = current_branch_name()
@@ -56,7 +58,7 @@ elif ch == 4:
         os.system('git pull origin main')
         os.system(f'git merge {branch_merge_name}')
         os.system('git push origin main')
-    print('Process completed successfully')      
+    print('Process completed successfully')
 
 else:
     print('Wrong choice')
