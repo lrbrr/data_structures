f92b66ebe7ec92dfaa9bc2dcf20e16345d036b64
sauravs <sauravshah.mail@gmail.com>
2022-06-21 22:33:25 +0545

    make pythonic

--- equal_stacks.py
+++ equal_stacks.py
@@ -1,17 +1,9 @@
 def equalStacks(h1, h2, h3):
     if len(h1) == 0 or len(h2) == 0 or len(h3) == 0: return 0
-    
-    h1_sum =[h1[-1]]
-    for i in range(len(h1)-2, -1, -1):
-        h1_sum.append(h1[i] + h1_sum[-1])
 
-    h2_sum =[h2[-1]]
-    for i in range(len(h2)-2, -1, -1):
-        h2_sum.append(h2[i] + h2_sum[-1])
-
-    h3_sum =[h3[-1]]
-    for i in range(len(h3)-2, -1, -1):
-        h3_sum.append(h3[i] + h3_sum[-1])
+    h1_sum = [sum((h1[::-1])[:i+1]) for i in range(len(h1))]
+    h2_sum = [sum((h2[::-1])[:i+1]) for i in range(len(h2))]
+    h3_sum = [sum((h3[::-1])[:i+1]) for i in range(len(h3))]
 
     smallest_array = h1_sum if len(h1_sum) < len(h2_sum) else h2_sum
     smallest_array = smallest_array if len(smallest_array) < len(h3_sum) else h3_sum
