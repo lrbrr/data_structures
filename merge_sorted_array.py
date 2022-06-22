e44e2b6fc76dc6dbce799189414a18175e8955bd
sauravs <sauravshah.mail@gmail.com>
2022-06-22 16:31:41 +0545

    improvement

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -5,25 +5,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if m == 0:
-            nums1[:] = nums2[:]
-        elif n == 0:
-            pass
-        else:
-            tracker1 = 0
-            tracker2 = 0
-            while tracker1 < m and tracker2 < n:
-                if nums1[tracker1] < nums2[tracker2]:
-                    tracker1 += 1
-                else:
-                    nums1.insert(tracker1, nums2[tracker2])
-                    tracker1 += 1
-                    tracker2 += 1
-                    m += 1
-                    nums1.pop(-1)
-            
-            if tracker2 < n:
-                nums1[tracker1:] = nums2[tracker2:]
+        tracker1 = 0
+        tracker2 = 0
+        while tracker1 < m and tracker2 < n:
+            if nums1[tracker1] < nums2[tracker2]:
+                tracker1 += 1
+            else:
+                nums1.insert(tracker1, nums2[tracker2])
+                tracker1 += 1
+                tracker2 += 1
+                m += 1
+                nums1.pop(-1)
+        
+        if tracker2 < n:
+            nums1[tracker1:] = nums2[tracker2:]
 
         print(nums1)
 
