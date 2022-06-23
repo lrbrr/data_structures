642aca9f206465ecea789f23650a9ab8d07682c0
sauravs <sauravshah.mail@gmail.com>
2022-06-23 12:27:34 +0545

    move to folder

--- multiples_of_3_or_5.py
+++
@@ -1,7 +0,0 @@
-# Calculate the sum of multiples of 3 or 5 between 1 and 100
-
-total = 0
-for i in range(1, 100):
-    if i % 3 == 0 or i % 5 == 0:
-        total += i
-print(total)
--- remove_palindromic_subsequences.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        return s == s[::-1]
-    def removePalindromeSub(self, s: str) -> int:
-        return 1 if self.isPalindrome(s) else 2
-
-print(Solution().removePalindromeSub("ababa")) # 1
-print(Solution().removePalindromeSub("aaabbabab")) # 2
